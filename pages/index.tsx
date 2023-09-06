f81f76aae76eb0bf99291aedaffe3cf104129cc7
taichi-de <61309248+taichi-de@users.noreply.github.com>
2023-09-06 10:23:43 +0200

    removed search feature

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,7 +25,6 @@ const Home: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
   categories,
   tags,
 }: Props) => {
-  // const [search, setSearch] = useState<Props>();
   const { setAllBlogs } = useAllBlogsState();
   const { setAllCategories } = useAllCategoriesState();
   const { setAllTags } = useAllTagsState();
@@ -41,52 +40,10 @@ const Home: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
     setShowBlogs(blogs);
   }, []);
 
-  // FIXME: Search function
-  // const handleSubmit: ComponentProps<"form">["onSubmit"] = async (e) => {
-  //   e.preventDefault();
-  //   const q = e.currentTarget.query.value;
-  //   const data = await fetch("/api/search", {
-  //     body: JSON.stringify({ q }),
-  //     headers: { "blog-Type": "application/json" },
-  //     method: "POST",
-  //   });
-  //   const json = await data.json();
-  //   setSearch(json);
-  // };
-
-  // const handleClick: ComponentProps<"button">["onClick"] = async (e) => {
-  //   setSearch(undefined);
-  // };
-
-  // const contents = search ? search.contents : props.contents;
-  // const totalCount = search ? search.totalCount : props.totalCount;
-
   return (
     <BlogLayout>
       <div className="grid grid-cols-12 gap-4 py-4 px-0 md:p-8 lg:w-4/5 w-full h-[90%] text-left">
         <div className="lg:col-span-8 col-span-12 bg-main pt-4 px-2 rounded-sm">
-          {/* <div className="md:flex block justify-between items-center px-4">
-            <p className="text-sub">{`${
-              search ? "Search result" : "Total"
-            }: ${totalCount} Posts`}</p>
-            <form className="flex gap-x-2" onSubmit={handleSubmit}>
-              <input
-                type="text"
-                name="query"
-                className="border rounded-md border-third p-1 focus:border-secondary"
-              />
-              <Button className="rounded-md px-1 text-main bg-third hover:opacity-80">
-                Search
-              </Button>
-              <Button
-                type="reset"
-                className="rounded-md px-1 text-main bg-third hover:opacity-80"
-                onClick={handleClick}
-              >
-                Reset
-              </Button>
-            </form>
-          </div> */}
           <CategoryTabs />
           <div className="grid md:grid-cols-2 gap-4 m-4">
             {showBlogs.slice(offset, offset + perPage).map((blog: Blog) => {
